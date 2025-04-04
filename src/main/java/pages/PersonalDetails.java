4ef245d58693da67a0baa4424a4239d8302eff1c
Nancy EL Sherbiny <103130605+NancELSherbiny@users.noreply.github.com>
2025-04-04 02:41:49 -0700

    Update PersonalDetails.java

--- src/main/java/pages/PersonalDetails.java
+++ src/main/java/pages/PersonalDetails.java
@@ -8,9 +8,8 @@ public class PersonalDetails extends PageBase {
     By MyInfo = By.xpath("(//*[@class=\"oxd-text oxd-text--span oxd-main-menu-item--name\"])[6]");
     By firstNameField = By.xpath("//*[@class=\"oxd-input oxd-input--active orangehrm-firstname\"]");
     By lastNameField = By.xpath("//*[@class=\"oxd-input oxd-input--active orangehrm-lastname\"]");
-    //By licenseExpiryDateField = By.xpath("//*[@name='licenseExpiryDate']");
     By saveButton = By.xpath("(//*[@class=\"oxd-button oxd-button--medium oxd-button--secondary orangehrm-left-space\"])[1]");
-
+   // By Nationality = By.xpath("(//*[@class=\"oxd-select-text-input\"])[1]");
     public PersonalDetails(WebDriver driver) {
         super(driver);
     }
@@ -25,6 +24,11 @@ public class PersonalDetails extends PageBase {
         clearField(lastNameField);
         sendKeys(lastNameField,name);
     }
+    
+    // public void chooseNationality(String nationality){
+    //     scrollDown("window.scrollBy(350,350)");
+    //     sendKeys(Nationality,nationality);
+    // }
 
     public void clickSave() {
         driver.findElement(saveButton).click();
